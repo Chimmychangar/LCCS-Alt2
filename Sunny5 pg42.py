2d18f6ac2477e220c383ac2e4dd7426a9f49364b
Chimmychangar <154215602+Chimmychangar@users.noreply.github.com>
2024-02-08 08:50:15 +0000

    Add files via upload

---
+++ Sunny5 pg42.py
@@ -0,0 +1,15 @@
+#5 x mars bars @ €1 each
+#4 x cans of coke @ €1.50 each
+#3 x bags of crisps @ 80 cents each
+#2 x cups of tea @ €2 euro each
+#1 x slice pan @ €3.50 each
+
+MarBar = 5
+CanCoke = 4*1.5
+BagCrisp = 3*0.8
+CupTea = 2*2
+SlicePan = 3.5
+#print(type(MarBar),type(CanCoke))
+# print(MarBar,"\n",CanCoke,)
+Total = round(MarBar+CanCoke+BagCrisp+CupTea+SlicePan,1)
+print(" The total cost is", Total)
---
+++ sunny6 work.py
@@ -0,0 +1,17 @@
+'''
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+sum = number1 + number2
+print("The answer is", sum)
+print(number1, "+", number2, "=", sum)
+'''
+
+t
+
+# A program to convert from Centigrade to Fahrenheit
+Fahrenheit = float(input("Enter the Farenheit value: "))
+Centigrade = 9/5 * Fahrenheit - 32
+print(Centigrade, "degrees C equals", Fahrenheit, "degrees F")
+
+
+
